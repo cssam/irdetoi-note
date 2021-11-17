aa63437bcce128415447aabecaf9821f270b83f5
Chandima Samaraweera <cssam1@hotmail.com>
2021-11-17 00:18:36 -0500

    node runtime version fixed

--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
+sudo apt install npm
+sudo npm install -g npm@latest
+sudo apt-get install -y nodejs
 sudo apt-get install update -y
+sudo npm install -g nginx
 sudo npm install pm2 -g
 sudo pm2 update
+sudo apt install build-essential
